9040a0185c2b76b1a98a9a68308f02d23a4f5eeb
bigeddie111 <edwinmyla2@gmail.com>
2024-05-17 12:26:45 -0400

    -

--- script.js
+++ script.js
@@ -22,34 +22,85 @@ const navTextNormal = document.querySelectorAll("#offcanvasNavbar ul li .non-dro
 const navOrder = document.querySelector("#offcanvasNavbar ul li .nav-item-order")
 const navDropdown = document.querySelector(".dropdown .dropdown-title");
 
+
+    
 window.addEventListener('scroll', () => {
-    if (window.scrollY >= 250) {
+    if (window.scrollY >= 250 && window.innerWidth >= 1000) {
+        navEl.style.marginTop = "0em";
         navEl.classList.add('fixed-top')
         navEl.classList.add('navbar-scrolled');
-        navImg.style.width = "14%";
-        navImg.style.marginBottom = "5em";
+        navImg.style.width = "17%";
+        navImg.style.marginBottom = "1em";
         navTextNormal.forEach(element => {
             element.style.color = "black";
+            element.addEventListener("mouseenter", () => {
+                element.style.color = "white";
+            })
+
+            element.addEventListener("mouseleave", () => {
+                element.style.color = "black";
+            })
         })
         navOrder.style.color = "black";
         navOrder.style.border = "solid 1px black";
         navDropdown.style.color = "black";
 
+        navDropdown.addEventListener("mouseenter", () => {
+            navDropdown.style.color = "white"
+        })
+
+        navDropdown.addEventListener("mouseleave", () => {
+            navDropdown.style.color = "black"
+        })
+
+        navOrder.addEventListener("mouseenter", () => {
+            navOrder.style.border = "1px solid white";
+        })
+
+        navOrder.addEventListener("mouseleave", () => {
+            navOrder.style.border = "1px solid black";
+            navOrder.style.color = "black";
+        })
+
 
     } else {
+        navEl.style.marginTop = "4em";
         navEl.classList.remove('fixed-top')
         navEl.classList.remove('navbar-scrolled');
         navImg.style.width = "30%";
         navImg.style.marginBottom = "0";        
         navTextNormal.forEach(element => {
             element.style.color = "white";
+            element.addEventListener("mouseenter", () => {
+                element.style.color = "rgb(202, 202, 202)";
+            })
+
+            element.addEventListener("mouseleave", () => {
+                element.style.color = "white";
+            })
         })
         navOrder.style.color = "white";
         navOrder.style.border = "solid 1px white";
         navDropdown.style.color = "white";
+        
+        navDropdown.addEventListener("mouseenter", () => {
+            navDropdown.style.color = "rgb(202, 202, 202)"
+        })
+
+        navDropdown.addEventListener("mouseleave", () => {
+            navDropdown.style.color = "white"
+        })
+        
+        navOrder.addEventListener("mouseenter", () => {
+            navOrder.style.color = "black";
+        })
+
+        navOrder.addEventListener("mouseleave", () => {
+            navOrder.style.border = "solid 1px white"
+            navOrder.style.color = "white";
+        })
+
+        
     }
 })
 
-function changeToBlack(element) {
-    element.style.color = "black";
-}
