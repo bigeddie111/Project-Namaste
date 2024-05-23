40b6fcd5da28a94492f1c8e42bd4efa2ec34ac3b
bigeddie111 <edwinmyla2@gmail.com>
2024-05-23 10:56:33 -0400

    - home page responsiveness done - footer changed

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let parallax = document.querySelector("header");
 
 window.addEventListener("scroll", () => {
-   console.log(window.pageYOffset)
     let offset = window.pageYOffset;
     parallax.style.backgroundPositionY = offset * .3 + "px";
 })
